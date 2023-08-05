ee8db0ca5a81a198249918084a77b412c849686b
jonitjoseph <jonitjoseph@gmail.com>
2023-08-06 03:11:41 +0530

    added navbar, logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+import Feed from "@/components/Feed";
+
 export default function Home() {
   return (
     <>
-      <section className="w-full flex-center flex-col">
-        <h1>Hello world!</h1>
+      <section className="w-full flex justify-center items-center flex-col">
+        <h1 className="mt-5 text-5xl sm:text-6xl text-gray-900">Stay curious.</h1>
+        <h3 className="mt-5 text-lg sm:text-xl text-gray-600 max-w-2xl">Discover blogs and stories from writers on any topic.</h3>
+        <Feed />
       </section>
     </>
   )
---
+++ components/Navbar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+
+export default function Navbar() {
+    const [toggleDropdown, setToggleDropdown] = useState(false);
+    return (
+        <nav className="flex justify-between items-center w-full mb-16 pt-4">
+            <Link href='/' className="flex justify-center items-center gap-2">
+                <Image src="/assets/images/logo.svg" alt="Logo" width={30} height={30} className="object-contain"></Image>
+                <p className="max-sm:hidden">Next Blogs</p>
+            </Link>
+
+            {/* Desktop Navigation */}
+            <div className='sm:flex hidden'>
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href='/create'>Create</Link>
+                    <button type='button'>Sign Out</button>
+                    <Link href='/profile'>Profile</Link>
+                </div>
+                <button className="mx-2">Signin</button>
+            </div>
+
+            {/* Mobile Navigation */}
+            <div className='sm:hidden flex relative'>
+                <div className='flex'>
+                    <button onClick={() => setToggleDropdown(!toggleDropdown)}>Dropdown</button>
+                    {toggleDropdown && (
+                        <div className='flex flex-col justify-end mt-3 absolute top-full right-0 w-full p-5 gap-2 min-w-[210px] bg-gray-100 rounded-lg'>
+                            <Link href='/create' onClick={() => setToggleDropdown(false)}>Create</Link>
+                            <Link href='/profile' onClick={() => setToggleDropdown(false)}>Profile</Link>
+                            <button type='button'
+                                onClick={() => { setToggleDropdown(false); }}>Sign Out
+                            </button>
+                        </div>
+                    )}
+                </div>
+                <button className="mx-2">Signin</button>
+            </div>
+
+        </nav>
+    )
+}
