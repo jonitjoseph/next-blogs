40b6028fd033cb74ddad8f14483ca3cac1953770
jonitjoseph <jonitjoseph@gmail.com>
2023-08-07 01:49:50 +0530

    modified feed component

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,5 +1,24 @@
+import Post from "./Post";
+
 export default function Feed() {
     return (
-        <>Feed</>
+        <>
+            <section className="mt-16 mx-auto w-full max-w-xl flex justify-center items-center flex-col gap-2">
+                <form className="relative w-full flex justify-center items-center">
+                    <input
+                        type="text"
+                        placeholder='Search'
+                        required
+                        className="m-4 p-4 block w-full rounded-full border text-center outline-none focus:bg-gray-50 focus:border-gray-300" />
+                </form>
+                <div className="m-4 flex flex-col gap-4">
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                    <Post />
+                </div>
+            </section>
+        </>
     )
 }
---
+++ components/Post.tsx
@@ -0,0 +1,7 @@
+export default function Post() {
+    return (
+        <>
+            <h1 className="text-lg">hello, blog posts go here</h1>
+        </>
+    )
+}
